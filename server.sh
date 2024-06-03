a2a2668eb14c11feebcc03a07f445df6c49a4326
Nicolai Søborg <git@xn--sb-lka.org>
2024-06-03 21:43:56 +0200

    Allow two players with same name
    Security is based on guessing $RANDOM - which we all know is impossible /s

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 shopt -s nullglob  # make sure `for blah in /*` doesn't match `.`
 
-GAME_DIR="/tmp/actungdebash"
+GAME_DIR="/tmp/actungdebash$RANDOM"
 PLAYERS_DIR="$GAME_DIR/players"
 
 # https://stackoverflow.com/a/13322667
@@ -12,15 +12,18 @@ read -r _{,} _ _ _ _ SERVER_IP _ < <(ip r g 1)
 # 1338: get state
 # 1339: input
 
+get() {
+  cat "$1" 2>/dev/null
+}
+
 game_state() {
   # prints lobby or game canvas
   if [ -f "$GAME_DIR/lobby" ]; then
     printf "\033cWaiting for players to ready up.\n\n\t\x1b[1mcurl %s:1337 | bash\x1b[0m\n\nPress \x1b[4mwasd\x1b[0m when connected, then \x1b[4mr\x1b[0m when ready.\n\n\n" "$SERVER_IP"
     for player in "$PLAYERS_DIR"/*;
     do
-      local player_info="$(cat "$player/color" 2> /dev/null) $(basename "$player")"
-      local input=$(cat "$player/input" 2> /dev/null)
-      if [ "$input" == "r" ]
+      local player_info="$(get "$player/color") $(get "$player/name")"
+      if [ "$(get "$player/input")" == "r" ]
       then
         echo -e "  ✅ READY               $player_info"
       else
@@ -39,7 +42,7 @@ game_state() {
 }
 
 client_server() {
-  while true ; do printf 'HTTP/1.1 200 OK\r\n\r\ndraw() { while :; do printf "\\x1b[;H"; nc -dN %s 1338; sleep 1; done }\ninput() { while :; do read -sn1 c && printf "$(whoami) $c" | nc -N %s 1339; done }\ndraw & input\n' "$SERVER_IP" "$SERVER_IP" | nc -Nl 1337 >/dev/null; done
+  while true ; do printf 'HTTP/1.1 200 OK\r\n\r\ndraw() { while :; do printf "\\x1b[;H"; nc -dN %s 1338; sleep 1; done }\ninput() { S="$RANDOM"; while :; do read -sn1 c && printf "$S $(whoami) $c" | nc -N %s 1339; done }\ndraw & input\n' "$SERVER_IP" "$SERVER_IP" | nc -Nl 1337 >/dev/null; done
 }
 
 state_server() {
@@ -51,20 +54,21 @@ input_server() {
   do
     local input=$(nc -dl 1339)
     IFS=' ' read -a input_parts <<< "$input"
-    local name=${input_parts[0]}
-    local direction=${input_parts[-1]}
+    local secret=$(echo "${input_parts[0]}" | sha256sum | head -c 12)
+    local name="${input_parts[1]}"
+    local direction="${input_parts[-1]}"
     if [[ "$direction" =~ ^(w|a|s|d|r)$ ]]
     then
       # Allow new players to join while in lobby:
       if [ -f "$GAME_DIR/lobby" ]; then
-        if mkdir "$PLAYERS_DIR/$name" 2>/dev/null; then
-          echo -n "$1" > "$PLAYERS_DIR/$name/color"
+        if mkdir "$PLAYERS_DIR/$secret" 2>/dev/null; then
+          echo -n "$name" > "$PLAYERS_DIR/$secret/name"
+          echo -n "$1" > "$PLAYERS_DIR/$secret/color"
           shift  # pop arg1 ($1) = next color is now first
         fi
       fi
-
-      # Ignore errors if dead people try moving (`$name/` doesn't exist when dead)
-      2>/dev/null echo -n "$direction" > "$PLAYERS_DIR/$name/input"
+      # Ignore errors if dead people try moving (`$secret/` doesn't exist when dead)
+      2>/dev/null echo -n "$direction" > "$PLAYERS_DIR/$secret/input"
     fi
   done
 }
@@ -79,10 +83,11 @@ game() {
     local playerstring=""
     for player in "$PLAYERS_DIR"/*;
       do
-        local input=$(cat "$player/input" 2> /dev/null)
-        local color=$(cat "$player/color" 2> /dev/null)
-        local x=$(cat "$player/x" 2> /dev/null || printf "5")
-        local y=$(cat "$player/y" 2> /dev/null || printf "5")
+        local name=$(get "$player/name")
+        local input=$(get "$player/input")
+        local color=$(get "$player/color")
+        local x=$(get "$player/x" || printf 5)
+        local y=$(get "$player/y" || printf 5)
 
         case "$input" in
           "w")
@@ -116,7 +121,7 @@ game() {
         canvas[$((y*20 + x))]="$color"
         printf "$x" > "$player/x"
         printf "$y" > "$player/y"
-        playerstring="$playerstring$color $(basename "$player")\n"
+        playerstring="$playerstring$color $name\n"
       done
 
     # PRINT MAP
@@ -136,11 +141,11 @@ create_field() {
     # assign random empty start location not too close to edge
     while true
     do
-      local x=$((2 + RANDOM % 17))
-      local y=$((2 + RANDOM % 17))
+      local x=$((2 + RANDOM % 16))
+      local y=$((2 + RANDOM % 16))
 
       if [ "${canvas[$((y*20 + x))]}" = "⬜" ]; then
-        canvas[$((y*20 + x))]="$(cat $player/color)"
+        canvas[$((y*20 + x))]="$(get $player/color)"
         echo -n "$x" > "$player/x"
         echo -n "$y" > "$player/y"
         break
@@ -165,11 +170,9 @@ lobby() {
 }
 
 win() {
-  # TODO: Move this to game_state
-  # clear
   for player in "$PLAYERS_DIR"/*;
   do
-    local player_info="$(cat "$player/color" 2> /dev/null) $(basename "$player")"
+    local player_info="$(get "$player/color") $(get "$player/name")"
     echo -e "\x1b[3;3H🏁 Congratulations \x1b[1m$player_info \x1b[5m🏆\x1b[0m You have won the game! 🏁"
   done
   sleep 8
